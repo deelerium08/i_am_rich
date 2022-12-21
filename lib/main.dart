d6f0f6731a05cf14324a4579f955bbaf94af9bf4
d.florentina.calapod <>
2022-12-21 12:55:52 +0100

    add-image-from-assets+modif-pubspec-yaml

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,14 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false, //hide debug banner
       home: Scaffold(
-        backgroundColor: Colors.grey[400],
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: const Text("I Am Rich"),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://images.pexels.com/photos/91216/pexels-photo-91216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
